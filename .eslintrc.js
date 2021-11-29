9634fe443d97a9b63ca69eec23fc717edd70f60a
banti377 <patelbanti27@yahoo.in>
2021-11-29 22:24:35 +0530

    extend eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'next/core-web-vitals',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/function-component-definition': 'off',
+    'arrow-body-style': 'off',
+    'react/jsx-filename-extension': [1,
+      {
+        extensions: [
+          '.tsx',
+        ],
+      },
+    ],
+  },
+};
